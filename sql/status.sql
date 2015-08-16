d7d8749c5153032a112e0d1af69d38b55ed69646
Brian Wu <bwu@appnexus.com>
2015-08-15 22:27:20 -0400

    Changes to sql

--- sql/status.sql
+++ sql/status.sql
@@ -5,5 +5,5 @@ CREATE TABLE `status` (
   `link` varchar(255) NOT NULL,
   `date` timestamp NOT NULL DEFAULT '1970-01-01 00:00:01',
   `deleted` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
